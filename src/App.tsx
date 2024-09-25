5b59a3ab80eae0ee5ee81d7a78110d6306be6db6
koloja <136496969+koloja@users.noreply.github.com>
2024-09-25 09:28:23 +0100

    use @ not ./

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
-import Container from './components/Contianer';
+import Container from '@/components/Contianer';
 
 const App: React.FC = () => {
     return (
